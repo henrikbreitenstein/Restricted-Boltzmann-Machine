c3df68cb9810dd4a1aeea04280b5f13d70ed7834
henrikbreitenstein <henrik.breitenstein@bremo.org>
2023-12-27 18:48:49 +0100

    Edit to lipkin hamiltonian.

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ warnings.filterwarnings("ignore", category=UserWarning)
 def lipkin_local(dist_s, uniform_s, eps, V, W):
     mask = torch.all(dist_s == uniform_s, dim=1)
     H_0 = torch.zeros_like(dist_s[:, 0])
-    H_0[mask] = 2*eps*(torch.sum(dist_s[mask] == 1, dim=1) - torch.sum(dist_s[mask] == 0, dim=1)).to(dtype=H_0.dtype)
+    H_0[mask] = eps*(torch.sum(dist_s[mask] == 1, dim=1) - torch.sum(dist_s[mask] == 0, dim=1)).to(dtype=H_0.dtype)
     doubles = torch.sum(dist_s-uniform_s, dim=1)
     doubles[torch.abs(doubles) != 2] = 0
-    H_1 = 0.5*V*torch.abs(doubles)
-    #H_1 = -V*(torch.abs(torch.sum(dist_s-uniform_s, dim=1)) == 2)
+    # H_1 = 0.5*V*torch.abs(doubles)
+    H_1 = -V*(torch.abs(torch.sum(dist_s-uniform_s, dim=1)) == 2)
     H_2 = W*(torch.abs(torch.sum(dist_s-uniform_s, dim=1)) == 1)
-    sum = H_0 + H_1
+    sum =  0.5*(H_0 + H_1)
     return sum
 
 if __name__ == '__main__':
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     init_model = model.set_up_model(visual_n, hidden_n, precision=precision, device=device, W_scale=0.1)
     
     eps = 1
-    v = 0.2
+    v = 0
     W = 0
     H = torch.tensor([[eps, 0, -v], [0, 0, 0], [-v, 0, -eps]], dtype=precision, device=device)
     eigvals = torch.real(torch.linalg.eigvals(H))  
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     
     epochs = 500
     learning_rate = 1
-    cycles = 100000
+    cycles = 1000
     k = 3
     local_energy = partial(lipkin_local, eps=eps, V=v, W=W)
     stats = solver.find_min_energy(init_model, local_energy, cycles, k, epochs , learning_rate)
